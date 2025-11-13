8abf407880c2542e2a4000d16236b85219f60fab
danandnad <57594790+danandnad@users.noreply.github.com>
2025-11-13 16:27:05 +0900

    Update and rename myContacts/config to myContacts/config / dbConnnet.js

---
+++ myContacts/config / dbConnnet.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//async, await //데이터베이스 접속하는 코드
+const dbConnect = async () =? {
+  try {
+    const connect = await mongoose.connect(process.env.DB_CONNECT);
+    console.log("DB Connected");
+  } catch(err) {
+      console.log(err);
+  }
+}
+
+
+module.exports = dbConnect;
